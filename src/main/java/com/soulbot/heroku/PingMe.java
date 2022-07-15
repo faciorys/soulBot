c55d0f11614129fec71a1b590f6f701f29decc6a
Harbachou Aliaksandr <aliaksandr.harbachou@mbicycle.com>
2022-07-15 17:02:43 +0300

    change PingMe.java

--- src/main/java/com/soulbot/heroku/PingMe.java
+++ src/main/java/com/soulbot/heroku/PingMe.java
@@ -15,7 +15,7 @@ public class PingMe {
     @SneakyThrows
     public void ping() {
         try {
-            URL url = new URL("https://soulmeowbot.herokuapp.com/");
+            URL url = new URL("https://google.com/");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
             log.info("Server is working...  Ping {} status: {}", url.getHost(), connection.getResponseCode());
